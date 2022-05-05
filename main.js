2e99c489a0218d8876cffc5664092f948c69d688
Nguyên <nguyenkhanhngoctran@gmail.com>
2022-05-06 00:11:56 +0700

    Update file main

--- main.js
+++ main.js
@@ -2,7 +2,27 @@ const jsonServer = require('json-server');
 const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './public',
+});
+const uniqid = require('uniqid');
+const multer = require('multer');
+const fs = require('fs');
+
+// Setup upload config
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const path = './public/posts';
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
+  },
+  filename: function (req, file, cb) {
+    const [fileName, fileExtension] = file.originalname.split('.');
+    cb(null, uniqid(`${fileName}-`, `.${fileExtension}`));
+  },
+});
+
+const upload = multer({ storage });
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -25,6 +45,58 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next();
 });
+
+function handleAddPost(req, res, next) {
+  const now = Date.now();
+
+  if (req.file?.filename) {
+    req.body.imageUrl = `${process.env.STATIC_URL}/posts/${req.file?.filename}`;
+  }
+
+  req.body.createdAt = now;
+  req.body.updatedAt = now;
+  next();
+}
+
+function handleUpdatePost(req, res, next) {
+  const now = Date.now();
+
+  if (req.file?.filename) {
+    req.body.imageUrl = `${process.env.STATIC_URL}/posts/${req.file?.filename}`;
+  }
+
+  req.body.updatedAt = now;
+  next();
+}
+
+function validateFormData(req, res, next) {
+  const contentType = req.headers['content-type'];
+  if (!contentType.includes('multipart/form-data')) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid Content-Type, only multipart/form-data is supported.' });
+  }
+
+  next();
+}
+
+function handleUploadFile(req, res, next) {
+  if (!['PATCH', 'POST'].includes(req.method)) {
+    return res.status(404).json({ error: 'Not Found' });
+  }
+
+  if (req.method === 'PATCH') return handleUpdatePost(req, res, next);
+
+  return handleAddPost(req, res, next);
+}
+
+server.use(
+  '/api/with-thumbnail',
+  validateFormData,
+  upload.single('image'),
+  handleUploadFile,
+  router
+);
 
 // Custom ouput for LIST with pagination
 router.render = (req, res) => {
